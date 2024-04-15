8b142d5eb8d012e21c677a8aa81fd94c6f10609b
Sandeep Singh <sandeepgaurdec13@gmail.com>
2024-04-15 09:19:19 +0530

    Merge Sort

---
+++ MergeSortArray.java
@@ -0,0 +1,26 @@
+public class MergeSortArray {
+    public static void main(String[] args) {
+        int[] num1 = new int[]{1,2,3,0,0,0};
+        int[] num2 = new int[]{2,5,6};
+        int m = 3;
+        int n = 3;
+        mergeSortedArray(num1,m,num2,n);
+        for(int i=0;i<num1.length;i++){
+            System.out.print(num1[i]);
+            System.out.printf(" ");
+        }
+    }
+
+    private static void mergeSortedArray(int[] nums1, int m, int[] nums2, int n) {
+        int p1=m-1;
+        int p2=n-1;
+        int i = m+n-1;
+        while(p2>=0){
+            if(p1 >=0 && nums1[p1] > nums2[p2]){
+                nums1[i--] = nums1[p1--];
+            } else{
+                nums1[i--] = nums2[p2--];
+            }
+        }
+    }
+}
--- out/production/LeetCode-150/README.md
+++ out/production/LeetCode-150/README.md
@@ -1,2 +1,7 @@
 # LeetCode-150
 Top Interview Question -150
+
+| Topic  | Question | Solution                                                                                     |
+|--------| -------- |----------------------------------------------------------------------------------------------|
+| Arrays ||                                                                                              |
+| 1      |Merge Sorted Array | [Solution](https://github.com/SandeepSinghGaur/LeetCode-150/blob/master/RemovalElement.java) |
